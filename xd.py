ea89b21d0c3d46a8fa9c63387f05dd49d97e5b27
ckenlin <96581920+ckenlin@users.noreply.github.com>
2022-12-12 19:30:57 +0800

    Update xd.py

--- xd.py
+++ xd.py
@@ -6,6 +6,24 @@ st.audio(audio_file.read())
 
 
 
+option = st.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone'))
+
+st.write('You selected:', option)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

19f9f5ff0ee3ac2a6f21a299f7717ebf369496fc
ckenlin <96581920+ckenlin@users.noreply.github.com>
2022-12-19 19:58:47 +0800

    Update xd.py

--- xd.py
+++ xd.py
@@ -4,9 +4,9 @@ audio_file = open("一心兄弟.mp3", "rb")
 st.audio(audio_file.read()) 
 
 
-name={'一心兄弟','cd','dd'}
+name={'一心兄弟.','cd','dd'}
 option= st.selectbox( '請選擇想聽的音樂',name)
-audio_file = open(name.mp3'', "rb")
+audio_file = open(name, "rb")
 st.audio(audio_file.read()) 
 
 

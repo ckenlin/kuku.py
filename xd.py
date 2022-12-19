41941b7f8de46270a01f93c3bf3619187af35365
ckenlin <96581920+ckenlin@users.noreply.github.com>
2022-12-19 20:39:33 +0800

    Update xd.py

--- xd.py
+++ xd.py
@@ -21,8 +21,7 @@ with st.sidebar:
 ################################
        
 if add_radio == '林':     
-    
-    st.header("A cat")
+    st.header("林")
     name={'一心兄弟','cd','dd'}        #放入歌名
     option= st.selectbox( '請選擇想聽的音樂',name)
     audio_file = open(option+'.mp3', "rb")
